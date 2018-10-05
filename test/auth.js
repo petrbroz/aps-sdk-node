ca0617e23d18421b6361b59a9ad6e5cdba8bfb57
Petr Broz <petr.broz@autodesk.com>
2018-10-05 17:37:14 +0200

    Added basic mocha tests.

---
+++ test/auth.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+const { AuthenticationClient } = require('..');
+
+describe('AuthenticationClient', function() {
+    beforeEach(function() {
+        const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
+        assert(FORGE_CLIENT_ID);
+        assert(FORGE_CLIENT_SECRET);
+        this.client = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
+    });
+
+    describe('authenticate()', function() {
+        it('should return a token for valid scopes', async function() {
+            const token = await this.client.authenticate(['viewables:read']);
+            assert(token.length > 0);
+        });
+
+        it('should reject promise for invalid scopes', function(done) {
+            this.client.authenticate(['bad:scope'])
+                .catch((err) => { done(); });
+        });
+    });
+});
