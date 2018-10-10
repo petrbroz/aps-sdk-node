2b9a50ef8e243bf34a86c6ea705f51038b6bb6d3
Petr Broz <petr.broz@autodesk.com>
2018-10-10 09:55:11 +0200

    Added a couple of model derivative APIs and tests.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { AuthenticationClient } = require('./auth');
 const { DataManagementClient } = require('./data');
+const { ModelDerivativeClient } = require('./deriv');
 
 module.exports = {
     AuthenticationClient,
-    DataManagementClient
+    DataManagementClient,
+    ModelDerivativeClient
 };
---
+++ test/deriv.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+const { AuthenticationClient, ModelDerivativeClient } = require('..');
+
+describe('ModelDerivativeClient', function() {
+    beforeEach(function() {
+        const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
+        assert(FORGE_CLIENT_ID);
+        assert(FORGE_CLIENT_SECRET);
+        const auth = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
+        this.client = new ModelDerivativeClient(auth);
+        this.urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6cGV0cmJyb3otZGF0YXNhbXBsZXMvYmFzaWMucnZ0';
+    });
+
+    describe('formats()', function() {
+        it('should return a list of formats', async function() {
+            const formats = await this.client.formats();
+            assert(formats);
+        });
+    });
+
+    describe('submitJob()', function() {
+        it('should return a job info', async function() {
+            const job = await this.client.submitJob(this.urn);
+            assert(job);
+            assert(job.result);
+        });
+    });
+});
