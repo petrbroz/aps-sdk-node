de106fa5f86a22582632e3ccb4e3d16086d4cd1c
Petr Broz <ipetrbroz@gmail.com>
2019-06-14 11:06:05 +0200

    Added BIM360 TypeScript definitions.

--- src/index.d.ts
+++ src/index.d.ts
@@ -371,4 +371,99 @@ declare module 'forge-nodejs-utils' {
          */
         getViewableProperties(urn: string, guid: string): Promise<IDerivativeProps>;
     }
+
+    interface IHub {
+        // TODO
+    }
+
+    interface IProject {
+        // TODO
+    }
+
+    interface IFolder {
+        // TODO
+    }
+
+    interface IItem {
+        // TODO
+    }
+
+    interface IVersion {
+        // TODO
+    }
+
+    /**
+     * Client providing access to Autodesk Forge
+     * {@link https://forge.autodesk.com/en/docs/bim360/v1|BIM360 APIs}.
+     */
+    class BIM360Client {
+        /**
+         * Initializes new client with specific authentication method.
+         * @param {object} [auth={token: FORGE_ACCESS_TOKEN}] Authentication object,
+         * containing either `client_id` and `client_secret` properties (for 2-legged authentication),
+         * or a single `token` property (for authentication with pre-generated access token).
+         * @param {string} [host="https://developer.api.autodesk.com"] Forge API host.
+         */
+        constructor(auth?: { token: string }, host?: string);
+
+        /**
+         * Gets a list of all hubs accessible to given credentials
+         * ({@link https://forge.autodesk.com/en/docs/data/v2/reference/http/hubs-GET|docs}).
+         * @async
+         * @returns {Promise<IHub[]>} List of hubs.
+         */
+        hubs(): Promise<IHub[]>;
+
+        /**
+         * Gets a hub with specific ID.
+         * @param {string} id Hub ID.
+         * @async
+         * @returns {Promise<IHub | null>} Hub or null if there isn't one.
+         */
+        hub(id: string): Promise<IHub | null>;
+
+        /**
+         * Gets a list of all projects in a hub.
+         * @param {string} hub Hub ID.
+         * @async
+         * @returns {Promise<IProject[]>} List of projects.
+         */
+        projects(hub: string): Promise<IProject[]>;
+
+        /**
+         * Gets a list of top folders in a project.
+         * @param {string} hub Hub ID.
+         * @param {string} project Project ID.
+         * @async
+         * @returns {Promise<IFolder[]>} List of folder records.
+         */
+        folders(hub: string, project: string): Promise<IFolder[]>;
+
+        /**
+         * Gets contents of a folder.
+         * @param {string} project Project ID.
+         * @param {string} folder Folder ID.
+         * @async
+         * @returns {Promise<IItem[]>} List of folder contents.
+         */
+        contents(project: string, folder: string): Promise<IItem[]>;
+
+        /**
+         * Gets versions of a folder item.
+         * @param {string} project Project ID.
+         * @param {string} item Item ID.
+         * @async
+         * @returns {Promise<IVersion[]>} List of item versions.
+         */
+        versions(project: string, item: string): Promise<IVersion[]>;
+
+        /**
+         * Gets "tip" version of a folder item.
+         * @param {string} project Project ID.
+         * @param {string} item Item ID.
+         * @async
+         * @returns {Promise<IVersion>} Tip version of the item.
+         */
+        tip(project: string, item: string): Promise<IVersion>;
+    }
 }
