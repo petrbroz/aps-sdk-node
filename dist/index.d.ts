52e0c5ad72d79323eae35eac9d556f3536a8f133
Petr Broz <ipetrbroz@gmail.com>
2019-10-09 16:39:02 +0200

    Updated docs for v7.1.1.

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './authentication';
-export * from './data-management';
-export * from './model-derivative';
-export * from './bim360';
-export * from './design-automation';
-//# sourceMappingURL=index.d.ts.map
