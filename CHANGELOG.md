0e14ced54be49ac51238eda9adff96bac72efc7e
Petr Broz <ipetrbroz@gmail.com>
2019-11-01 14:58:51 +0100

    Fixed missing '/' in 3-legged redirect URL.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Missing '/' in 3-legged redirect URL.
+
 ## [7.2.1] - 2019-10-17
 
 ### Fixed
--- src/authentication.ts
+++ src/authentication.ts
@@ -114,7 +114,7 @@ export class AuthenticationClient {
      * @returns {string} Autodesk login URL.
      */
     getAuthorizeRedirect(scopes: string[], redirectUri: string): string {
-        return `${this.host}${RootPath}/authorize?response_type=code&client_id=${this.client_id}&redirect_uri=${redirectUri}&scope=${scopes.join(' ')}`;
+        return `${this.host}/${RootPath}/authorize?response_type=code&client_id=${this.client_id}&redirect_uri=${redirectUri}&scope=${scopes.join(' ')}`;
     }
 
     /**
